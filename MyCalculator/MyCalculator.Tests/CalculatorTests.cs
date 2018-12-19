835c9ad2f6e71bf60546c50fb1efd82ee5a9b48c
brysonsherman <bryson.sherman@workday.com>
2018-12-19 01:30:53 -0800

    pushing party 1 of assignment 9

---
+++ MyCalculator/MyCalculator.Tests/CalculatorTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace MyCalculator.Tests
+{
+    public class EmptyClass
+    {
+        public EmptyClass()
+        {
+        }
+    }
+}
---
+++ MyCalculator/MyCalculator/Calculator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace MyCalculator
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+        }
+    }
+}
